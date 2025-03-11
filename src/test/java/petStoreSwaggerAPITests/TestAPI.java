a82755de3ba69f43771a32b994007f0a62a3167d
ZenabG <zenabkale@gmail.com>
2025-03-11 17:03:32 +0100

    switched to master

--- src/test/java/petStoreSwaggerAPITests/TestAPI.java
+++ src/test/java/petStoreSwaggerAPITests/TestAPI.java
@@ -17,24 +17,21 @@ import Utils.JsonUtility;
 /**
  * This class contains test methods for Pet Store API.
  */
-public class TestAPI extends ExtentReportSetUp {
+public class TestAPI extends APIConnectionSetUp {
 
 	List<Long> petIDs = new ArrayList<>();
 	JsonUtility jsonUtility = new JsonUtility();
 
-	APIConnectionSetUp apiConnectionSetUp = new APIConnectionSetUp();
-
 	/**
 	 * Test to get all available pet details.
 	 *
-	 * @throws IOException
 	 * @throws InterruptedException
 	 * @throws ParseException
 	 */
 	@Test(priority = 0)
-	public void testGetAllAvailablePetDetails() throws IOException, InterruptedException, ParseException {
+	public void testGetAllAvailablePetDetails() throws InterruptedException {
 		System.out.println("Starting test: Get all available pet details");
-		String response = apiConnectionSetUp.pullRequest(Constants.PetStoreAPI + Constants.GetPetsByStatus + Constants.AvailablePetStatus);
+		String response = pullRequest(Constants.PetStoreAPI + Constants.GetPetsByStatus + Constants.AvailablePetStatus);
 		Assert.assertFalse(response.equals("[]") || response.isEmpty());
 
 		Map<Long, String> petIDNameMap = jsonUtility.getPetIDAndNameFromResponseJson(response);
@@ -49,14 +46,13 @@ public class TestAPI extends ExtentReportSetUp {
 	/**
 	 * Test to find a pet by its ID.
 	 *
-	 * @throws IOException
 	 * @throws InterruptedException
 	 * @throws ParseException
 	 */
 	@Test(priority = 1)
-	public void testFindPetByID() throws IOException, InterruptedException, ParseException {
+	public void testFindPetByID() throws InterruptedException {
 		System.out.println("Starting test: Find pet by ID");
-		String response = apiConnectionSetUp.pullRequest(Constants.PetStoreAPI + Constants.PetIdStatus + petIDs.get(0));
+		String response = pullRequest(Constants.PetStoreAPI + Constants.PetIdStatus + petIDs.get(0));
 		Assert.assertFalse(response.equals("[]") || response.isEmpty());
 		System.out.println("Completed test: Find pet by ID");
 	}
@@ -64,15 +60,14 @@ public class TestAPI extends ExtentReportSetUp {
 	/**
 	 * Test to create a new pet.
 	 *
-	 * @throws IOException
 	 * @throws InterruptedException
 	 * @throws ParseException
 	 */
 	@Test(priority = 2)
-	public void testCreatePet() throws IOException, InterruptedException, ParseException {
+	public void testCreatePet() throws InterruptedException {
 		System.out.println("Starting test: Create a new pet");
 		String petJson = "{\"id\": 12345, \"name\": \"doggie\", \"status\": \"available\"}";
-		String response = apiConnectionSetUp.postRequest(Constants.PetStoreAPI + Constants.PetEndpoint, petJson);
+		String response = postRequest(Constants.PetStoreAPI + Constants.PetEndpoint, petJson);
 		Assert.assertFalse(response.isEmpty());
 
 		// Extract the ID of the created pet and add it to the list
@@ -85,16 +80,15 @@ public class TestAPI extends ExtentReportSetUp {
 	/**
 	 * Test to update an existing pet.
 	 *
-	 * @throws IOException
 	 * @throws InterruptedException
 	 * @throws ParseException
 	 */
 	@Test(priority = 3)
-	public void testUpdatePet() throws IOException, InterruptedException, ParseException {
+	public void testUpdatePet() throws InterruptedException {
 		System.out.println("Starting test: Update an existing pet");
 		Long petIdToUpdate = petIDs.get(0);
 		String updatedPetJson = "{\"id\": " + petIdToUpdate + ", \"name\": \"updatedDoggie\", \"status\": \"sold\"}";
-		String response = apiConnectionSetUp.putRequest(Constants.PetStoreAPI + Constants.PetEndpoint, updatedPetJson);
+		String response = putRequest(Constants.PetStoreAPI + Constants.PetEndpoint, updatedPetJson);
 		Assert.assertFalse(response.isEmpty());
 		Assert.assertTrue(response.contains("updatedDoggie"));
 		System.out.println("Completed test: Update an existing pet");
@@ -103,14 +97,13 @@ public class TestAPI extends ExtentReportSetUp {
 	/**
 	 * Test to delete a pet by its ID.
 	 *
-	 * @throws IOException
 	 * @throws InterruptedException
 	 */
 	@Test(priority = 4)
-	public void testDeletePet() throws IOException, InterruptedException {
+	public void testDeletePet() throws InterruptedException {
 		System.out.println("Starting test: Delete a pet by ID");
 		Long petIdToDelete = petIDs.get(0);
-		String response = apiConnectionSetUp.deleteRequest(Constants.PetStoreAPI + Constants.PetIdStatus + petIdToDelete);
+		String response = deleteRequest(Constants.PetStoreAPI + Constants.PetIdStatus + petIdToDelete);
 		Assert.assertTrue(response.contains("200"));
 		System.out.println("Completed test: Delete a pet by ID");
 	}
